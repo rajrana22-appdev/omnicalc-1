12adf720868a7c017e3820510cece642f5df0e75
Raj Rana <rajrana22@uchicago.edu>
2022-04-22 06:33:05 +0000

    More

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,33 @@ class ApplicationController < ActionController::Base
     @square_of_num = @num ** 2
     render({ :template => "calculation_templates/square_results.html.erb" })
   end
+
+  def blank_root_form
+    render({ :template => "calculation_templates/root_form.html.erb" })
+  end
+
+  def calculate_root
+    @num = params.fetch("number").to_f
+    @root_of_num = Math.sqrt(@num)
+    render({ :template => "calculation_templates/root_results.html.erb" })
+  end
+
+  def blank_payment_form
+    render({ :template => "calculation_templates/payment_form.html.erb" })
+  end
+
+  def calculate_payment
+    render({ :template => "calculation_templates/payment_results.html.erb" })
+  end
+
+  def blank_random_form
+    render({ :template => "calculation_templates/rand_form.html.erb" })
+  end
+
+  def calculate_random
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
+    render({ :template => "calculation_templates/rand_results.html.erb" })
+  end
 end
